c3eaf1623d2d140258b2b6e025986b17e5372965
Khyojung <918fall@gamil.com>
2018-11-02 14:22:57 +0900

    first commit start project of quest

---
+++ app/src/main/java/com/example/hyojung/quest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hyojung.quest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
